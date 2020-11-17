69bab39ca6e7da2396c82215ed8f3ba3ecdfef28
reynazuno <364081@colum.edu>
2020-11-17 00:33:33 -0600

    js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $(".open").on("click", function() {
     $(".popup-overlay, .popup-content").addClass("active");
   });
   
-  
   $(".close, .popup-overlay").on("click", function() {
     $(".popup-overlay, .popup-content").removeClass("active");
   });
